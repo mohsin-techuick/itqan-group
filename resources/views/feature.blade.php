9822178e780527aad7c6b8f75fe951b9000edb3f
xeikh11 <51857159+xeikh11@users.noreply.github.com>
2020-11-13 15:59:50 +0500

    minor adjustments

--- resources/views/feature.blade.php
+++ resources/views/feature.blade.php
@@ -5,25 +5,25 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-3">
+    <div class="row" style="width:80%; margin-left:auto; margin-right:auto">
+        <div class="col-md-6">
             <div class="feature-text pt-3">
                 <h1> <span>BEST<span>FEATURES </h1>
                 <p class="lead">As Lead Consultant and Architect of Record inside The Kingdom of Saudi Arabia, we are providing our clients with all Architectural and Engineering Services required for a One-Stop-Consultancy, covering all project Phases such as pre-Concept, Concept, SchematicDesign & Design Development, Detailed Design, Tender Documentation and Poject Management & Site Supervision </p>
             </div>
             <div class="feature-list">
                 <ul>
-                    <li><a href="">Optimism</a></li>
-                    <li><a href="">Persistance</a></li>
-                    <li><a href="">Vision</a></li>
-                    <li><a href="">Innovativess</a></li>
-                    <li><a href="">Focus</a></li>
+                    <li>Optimism</a></li>
+                    <li>Persistance</a></li>
+                    <li>Vision</a></li>
+                    <li>Innovativess</a></li>
+                    <li>Focus</a></li>
                 </ul>
             </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-6">
             <div class="feature-image pt-3 text-right">
-                <img src="{{asset("images/feature.png")}}" alt="feature image">
+                <img src="{{asset("images/feature.png")}}" alt="feature image" width="100%">
             </div>
         </div>
     </div>
