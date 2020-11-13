2d5b87191452c50dcdfc1f8eba779f41b470bf9c
xeikh11 <51857159+xeikh11@users.noreply.github.com>
2020-11-13 17:52:25 +0500

    Final adjustments

--- resources/views/feature.blade.php
+++ resources/views/feature.blade.php
@@ -5,26 +5,26 @@
 @section('content')
 
 <div class="container">
-    <div class="row" style="width:80%;margin-left:auto;margin-right:auto">
-        <div class="col-md-4">
-            <div class="feature-text pt-3">
-                <img src="{{asset("images/arrow.png")}}">
+    <div class="row" style="width:75%;margin-left:auto;margin-right:auto">
+        <div class="col-md-6">
+            <div class="feature-text pt-3" style="margin-top:5px;">
+                <!-- <img src="{{asset("images/arrow.png")}}"> -->
                 <h1> <span>BEST<span>FEATURES </h1>
                 <p class="lead">As Lead Consultant and Architect of Record inside The Kingdom of Saudi Arabia, we are providing our clients with all Architectural and Engineering Services required for a One-Stop-Consultancy, covering all project Phases such as pre-Concept, Concept, SchematicDesign & Design Development, Detailed Design, Tender Documentation and Poject Management & Site Supervision </p>
             </div>
             <div class="feature-list">
                 <ul>
-                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Optimism</a></li>
-                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Persistance</a></li>
-                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Vision</a></li>
-                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Innovativess</a></li>
-                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Focus</a></li>
+                    <li><img src="{{asset("images/arrow.png")}}" style="margin-right:5px;">Optimism</li>
+                    <li><img src="{{asset("images/arrow.png")}}" style="margin-right:5px;">Persistance</li>
+                    <li><img src="{{asset("images/arrow.png")}}" style="margin-right:5px;">Vision</li>
+                    <li><img src="{{asset("images/arrow.png")}}" style="margin-right:5px;">Innovativess</li>
+                    <li><img src="{{asset("images/arrow.png")}}" style="margin-right:5px;">Focus</li>
                 </ul>
             </div>
         </div>
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="feature-image pt-3 text-right">
-                <img src="{{asset("images/feature.png")}}" alt="feature image">
+                <img src="{{asset("images/feature.png")}}" alt="feature image" width="100%">
             </div>
         </div>
     </div>
