9f6ce703d22974cf8ed0d4e8081be9ad16e52493
mohsin <mohsin.techuick@gmail.com>
2020-11-13 17:22:22 +0500

    adjust font size and some margin padding

--- resources/views/feature.blade.php
+++ resources/views/feature.blade.php
@@ -6,22 +6,23 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="feature-text pt-3">
+                <img src="{{asset("images/arrow.png")}}">
                 <h1> <span>BEST<span>FEATURES </h1>
                 <p class="lead">As Lead Consultant and Architect of Record inside The Kingdom of Saudi Arabia, we are providing our clients with all Architectural and Engineering Services required for a One-Stop-Consultancy, covering all project Phases such as pre-Concept, Concept, SchematicDesign & Design Development, Detailed Design, Tender Documentation and Poject Management & Site Supervision </p>
             </div>
             <div class="feature-list">
                 <ul>
-                    <li><a href="#">Optimism</a></li>
-                    <li><a href="#">Persistance</a></li>
-                    <li><a href="#">Vision</a></li>
-                    <li><a href="#">Innovativess</a></li>
-                    <li><a href="#">Focus</a></li>
+                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Optimism</a></li>
+                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Persistance</a></li>
+                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Vision</a></li>
+                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Innovativess</a></li>
+                    <li><img src="{{asset("images/arrow.png")}}"><a href="#">Focus</a></li>
                 </ul>
             </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-8">
             <div class="feature-image pt-3 text-right">
                 <img src="{{asset("images/feature.png")}}" alt="feature image">
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <link rel="stylesheet" href="{{asset("css/custom.css")}}">
     <!--Custom fonts-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@800&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
